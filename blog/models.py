1b74435afc817af802524ec69420dacd0a45ac35
Adiele Acha <achasongs@gmail.com>
2024-01-08 05:39:44 +0100

    weekend codes

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('ar ticle-detail', args=(str(self.id)))
+        return reverse('blog')
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default ="Our blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='events')
+
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 # from .import views
-from .views import BlogHome, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import BlogHome, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
 
 urlpatterns = [
     path('', BlogHome.as_view(), name="blog"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
     path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('add_category/', AddCategoryView.as_view(), name="add_category"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'), 
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'), 
 ]
